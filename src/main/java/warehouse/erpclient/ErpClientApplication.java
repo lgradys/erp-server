4160902995aa84d4cf91479da58cbf2ee1295792
lukasz <lukasz.piotr.gradys@gmail.com>
2022-01-10 18:10:49 +0100

    Initial commit

---
+++ src/main/java/warehouse/erpclient/ErpClientApplication.java
@@ -0,0 +1,13 @@
+package warehouse.erpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/warehouse/erpclient/ErpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package warehouse.erpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
